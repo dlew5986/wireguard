f4d53fb3232100b4615616d2be1fb29916e2945e
dlew5986 <19508775+dlew5986@users.noreply.github.com>
2022-11-13 13:46:02 -0500

    add terraform to build ec2 instance

---
+++ terraform/ec2_security_group.tf
@@ -0,0 +1,26 @@
+data "aws_vpc" "vpc_default" {
+  default = true
+}
+
+resource "aws_security_group" "wireguard" {
+  name   = local.security_group_name
+  vpc_id = data.aws_vpc.vpc_default.id
+
+  # allow ICMPv4 echo
+  ingress {
+    description = "ICMPv4 echo"
+    from_port   = 8
+    to_port     = 0
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # allow all outbound
+  egress {
+    description = "all outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
---
+++ terraform/iam_instance_profile.tf
@@ -0,0 +1,37 @@
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "wireguard" {
+  name               = local.session_manager_role_name
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+}
+
+resource "aws_iam_instance_profile" "wireguard" {
+  name = local.session_manager_instance_profile_name
+  role = aws_iam_role.session_manager.name
+}
+
+data "aws_iam_policy" "ssm_agent" {
+  arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_agent" {
+  role       = aws_iam_role.session_manager.name
+  policy_arn = data.aws_iam_policy.ssm_agent.arn
+}
+
+data "aws_iam_policy" "cloudwatch_agent" {
+  arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "cloudwatch_agent" {
+  role       = aws_iam_role.session_manager.name
+  policy_arn = data.aws_iam_policy.cloudwatch_agent.arn
+}
